d74f99f170bc9bccf0978de89ffdd20f13b07f0d
Lijiang Long <llong35@gatech.edu>
2020-04-03 15:45:15 -0400

    test

--- Utils/temp.py
+++ Utils/temp.py
@@ -1,8 +1,15 @@
 #transfer
 import os
+import subprocess
 source = '/data/home/llong35/data/annotated_videos'
-target = '/data/home/llong35/data/all_videos'
+target = '/data/home/llong35/data/all_videos/'
 
 for folder in os.listdir(source):
     folder_path = os.path.join(source,folder)
-    print(folder_path)
+    videos = os.listdir(folder_path):
+        for video in videos:
+            if not video.endswith('.mp4'):
+                continue
+            video_path = os.path.join(folder_path,video)
+            command = ['cp',video_path,target]
+            subprocess.run(command)
