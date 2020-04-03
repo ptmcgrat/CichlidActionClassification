2270379e5dfed07f290279f76d6b4644442d1b45
Lijiang Long <llong35@gatech.edu>
2020-04-03 15:46:16 -0400

    test

--- Utils/temp.py
+++ Utils/temp.py
@@ -6,10 +6,10 @@ target = '/data/home/llong35/data/all_videos/'
 
 for folder in os.listdir(source):
     folder_path = os.path.join(source,folder)
-    videos = os.listdir(folder_path):
-        for video in videos:
-            if not video.endswith('.mp4'):
-                continue
-            video_path = os.path.join(folder_path,video)
-            command = ['cp',video_path,target]
-            subprocess.run(command)
+    videos = os.listdir(folder_path)
+    for video in videos:
+        if not video.endswith('.mp4'):
+            continue
+        video_path = os.path.join(folder_path,video)
+        command = ['cp',video_path,target]
+        subprocess.run(command)
