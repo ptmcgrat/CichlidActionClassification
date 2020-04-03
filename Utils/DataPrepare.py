82f0791965365c0ba3ff9020d2682a84eeec2b16
Lijiang Long <llong35@gatech.edu>
2020-04-03 16:30:13 -0400

    test

--- Utils/DataPrepare.py
+++ Utils/DataPrepare.py
@@ -20,6 +20,21 @@ class DP_worker():
             cmd = ['ffmpeg','-i',video_file_path,target_folder+'/image_%05d.jpg']
             subprocess.run(cmd)
             break
+        
+        #count number of frames
+        for video in os.listdir(videos_temp):
+            video_folder = os.path.join(videos_temp,video)
+            image_indices = []
+            for image_file_name in os.listdir(video_folder):
+                if 'image' not in image_file_name:
+                    continue
+                image_indices.append(int(image_file_name[6:11]))
+            image_indices.sort(reverse=True)
+            n_frames = image_indices[0]
+            with open(os.path.join(video_folder, 'n_frames'), 'w') as dst_file:
+                dst_file.write(str(n_frames))
+            break
+        
             
         #calculate means
         
