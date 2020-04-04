d6932336645779bd5b4007f68181b23c36ecf1a2
Lijiang Long <llong35@gatech.edu>
2020-04-04 18:28:06 -0400

    test

--- Utils/DataPrepare.py
+++ Utils/DataPrepare.py
@@ -26,62 +26,62 @@ class DP_worker():
             annotation_file  = os.path.join(self.args.Log_directory,'target_domain_annotation.csv')
             meansalll_file = os.path.join(self.args.Log_directory,'target_MeansAll.csv')
             means_file = os.path.join(self.args.Log_directory,'target_Means.csv')
-#             annotation_f=open(annotation_file,'w')
-#             print('Location,MeanID', file = annotation_f)
+            annotation_f=open(annotation_file,'w')
+            print('Location,MeanID', file = annotation_f)
         
         videos_temp = os.path.join(self.args.Clips_temp_directory,domain)
         if not os.path.exists(videos_temp):
             os.makedirs(videos_temp)
         
-#         for file_name in os.listdir(video_dir):
-#             if not file_name.endswith('.mp4'):
-#                 continue
-#             if domain == 'source':
-#                 location = file_name.split('.')[0]
-#             else:
-#                 tokens = file_name.split('.')[0].split('__')
-#                 location = '_'.join(tokens[-5:])
-#                 MeanID = ':'.join(tokens[:2])
-#                 print(location+','+MeanID,file=annotation_f)
-#             video_file_path = os.path.join(video_dir,file_name)
-#             target_folder = os.path.join(videos_temp,location)
-#             if not os.path.exists(target_folder):
-#                 os.makedirs(target_folder)
-#             cmd = ['ffmpeg','-i',video_file_path,target_folder+'/image_%05d.jpg']
-#             subprocess.run(cmd)
-#         if domain == 'target':
-#             annotation_f.close()
+        for file_name in os.listdir(video_dir):
+            if not file_name.endswith('.mp4'):
+                continue
+            if domain == 'source':
+                location = file_name.split('.')[0]
+            else:
+                tokens = file_name.split('.')[0].split('__')
+                location = '_'.join(tokens[-5:])
+                MeanID = ':'.join(tokens[:2])
+                print(location+','+MeanID,file=annotation_f)
+            video_file_path = os.path.join(video_dir,file_name)
+            target_folder = os.path.join(videos_temp,location)
+            if not os.path.exists(target_folder):
+                os.makedirs(target_folder)
+            cmd = ['ffmpeg','-i',video_file_path,target_folder+'/image_%05d.jpg']
+            subprocess.run(cmd)
+        if domain == 'target':
+            annotation_f.close()
             
-#         with open(meansalll_file, 'w') as f:
-#             print('Clip,MeanR,MeanG,MeanB,StdR,StdG,StdB', file = f)
-#             for video in os.listdir(videos_temp):
-#                 video_folder = os.path.join(videos_temp,video)
-#                 image_indices = []
-#                 frames = []
-#                 for image_file_name in os.listdir(video_folder):
-#                     image_file_path = os.path.join(video_folder,image_file_name)
-#                     if 'image' not in image_file_name:
-#                         continue
-#                     image_indices.append(int(image_file_name[6:11]))
-#                     frames.append(image_file_path)
-#                 image_indices.sort(reverse=True)
-#                 n_frames = image_indices[0]
-#                 with open(os.path.join(video_folder, 'n_frames'), 'w') as dst_file:
-#                     dst_file.write(str(n_frames))
-#                 img = io.imread(frames[0])
-#                 mean = img.mean(axis = (0,1))
-#                 std = img.std(axis = (0,1))
-#                 print(video + ',' + ','.join([str(x) for x in mean]) + ',' + ','.join([str(x) for x in std]), file = f)
-#         dt = pd.read_csv(meansalll_file,sep=',')
+        with open(meansalll_file, 'w') as f:
+            print('Clip,MeanR,MeanG,MeanB,StdR,StdG,StdB', file = f)
+            for video in os.listdir(videos_temp):
+                video_folder = os.path.join(videos_temp,video)
+                image_indices = []
+                frames = []
+                for image_file_name in os.listdir(video_folder):
+                    image_file_path = os.path.join(video_folder,image_file_name)
+                    if 'image' not in image_file_name:
+                        continue
+                    image_indices.append(int(image_file_name[6:11]))
+                    frames.append(image_file_path)
+                image_indices.sort(reverse=True)
+                n_frames = image_indices[0]
+                with open(os.path.join(video_folder, 'n_frames'), 'w') as dst_file:
+                    dst_file.write(str(n_frames))
+                img = io.imread(frames[0])
+                mean = img.mean(axis = (0,1))
+                std = img.std(axis = (0,1))
+                print(video + ',' + ','.join([str(x) for x in mean]) + ',' + ','.join([str(x) for x in std]), file = f)
+        dt = pd.read_csv(meansalll_file,sep=',')
         annotation_df = pd.read_csv(annotation_file,sep=',')
-#         dt['MeanID'] = dt.apply(lambda row: annotation_df.loc[annotation_df.Location==row.Clip].MeanID.values[0], axis = 1)
-#         means = dt.groupby('MeanID').mean()
-#         
-#         with open(means_file,'w') as f:
-#             print('meanID,redMean,greenMean,blueMean,redStd,greenStd,blueStd', file = f)
-#             for row in means.itertuples():
-#                 print(row.Index + ',' + str(row.MeanR) + ',' + str(row.MeanG) + ',' + str(row.MeanB) + ',' + str(row.StdR) + ',' + str(row.StdG) + ',' + str(row.StdB), file = f)
-#         
+        dt['MeanID'] = dt.apply(lambda row: annotation_df.loc[annotation_df.Location==row.Clip].MeanID.values[0], axis = 1)
+        means = dt.groupby('MeanID').mean()
+        
+        with open(means_file,'w') as f:
+            print('meanID,redMean,greenMean,blueMean,redStd,greenStd,blueStd', file = f)
+            for row in means.itertuples():
+                print(row.Index + ',' + str(row.MeanR) + ',' + str(row.MeanG) + ',' + str(row.MeanB) + ',' + str(row.StdR) + ',' + str(row.StdG) + ',' + str(row.StdB), file = f)
+        
         if domain == 'source':
             train_list = os.path.join(self.args.Log_directory,'source_train_list.txt')
             val_list = os.path.join(self.args.Log_directory,'source_val_list.txt')
@@ -162,8 +162,8 @@ class DP_worker():
     def work(self):
         #convert to jpegs
 #         self.prepare_domain('source')
-        self.prepare_domain('target')
-#         self.prepare_json()
+#         self.prepare_domain('target')
+        self.prepare_json()
 #         
         
             
