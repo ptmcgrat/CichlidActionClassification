04ebc14efa9639163024bfd8dbb37c9a3704db01
Lijiang Long <llong35@gatech.edu>
2020-04-04 20:31:58 -0400

    test

--- Utils/transforms.py
+++ Utils/transforms.py
@@ -250,6 +250,33 @@ class FixedScaleRandomCenterCrop(object):
 #        self.offset_x = random.randint(-1*int(self.size/4), int(self.size/4))
 #        self.offset_y = random.randint(-1*int(self.size/4), int(self.size/4))
 
+class MultiScaleRandomCenterCrop(object):
+
+    def __init__(self, scales, size, interpolation=Image.BILINEAR):
+        self.scales = scales
+        self.size = size
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        crop_size = int(self.size * self.scale)
+
+        image_width = img.size[0]
+        image_height = img.size[1]
+
+        x1 = self.tl_x * (image_width - crop_size)
+        y1 = self.tl_y * (image_height - crop_size)
+        x2 = x1 + crop_size
+        y2 = y1 + crop_size
+
+        img = img.crop((x1, y1, x2, y2))
+
+        return img.resize((self.size, self.size), self.interpolation)
+
+    def randomize_parameters(self):
+        self.scale = self.scales[random.randint(0, len(self.scales) - 1)]
+        self.tl_x = random.random()
+        self.tl_y = random.random()
+
 
 class TargetCompose(object):
 
