34e59ff5c0df67f8b374e97ed89895e9c535ddeb
Lijiang Long <llong35@gatech.edu>
2020-08-07 16:54:21 -0400

    changed data preprocess such that one the processed file exists, the script will not run

--- Utils/DataPrepare.py
+++ Utils/DataPrepare.py
@@ -21,7 +21,7 @@ class DP_worker():
 
         if not os.path.exists(videos_temp):
             os.makedirs(videos_temp)
-        count = 0
+        print('convert video clips to images for faster loading')
         for file_name in os.listdir(video_dir):
             if not file_name.endswith('.mp4'):
                 continue
@@ -32,58 +32,61 @@ class DP_worker():
                 os.makedirs(target_folder)
                 cmd = ['ffmpeg','-i',video_file_path,target_folder+'/image_%05d.jpg']
                 subprocess.run(cmd)
-            count += 1
-        with open(means_all_file, 'w') as f:
-            print('Clip,MeanR,MeanG,MeanB,StdR,StdG,StdB', file = f)
-            for video in os.listdir(videos_temp):
-                video_folder = os.path.join(videos_temp,video)
-                image_indices = []
-                frames = []
-                for image_file_name in os.listdir(video_folder):
-                    image_file_path = os.path.join(video_folder,image_file_name)
-                    if 'image' not in image_file_name:
-                        continue
-                    image_indices.append(int(image_file_name[6:11]))
-                    frames.append(image_file_path)
-                image_indices.sort(reverse=True)
-                n_frames = image_indices[0]
-                with open(os.path.join(video_folder, 'n_frames'), 'w') as dst_file:
-                    dst_file.write(str(n_frames))
-                img = io.imread(frames[0])
-                mean = img.mean(axis = (0,1))
-                std = img.std(axis = (0,1))
-                print(video + ',' + ','.join([str(x) for x in mean]) + ',' + ','.join([str(x) for x in std]), file = f)
-        dt = pd.read_csv(means_all_file,sep=',')
-        annotation_df = pd.read_csv(annotation_file,sep=',')
-        dt['MeanID'] = dt.apply(lambda row: annotation_df.loc[annotation_df.Location==row.Clip].MeanID.values[0], axis = 1)
-        means = dt.groupby('MeanID').mean()
-        
-        with open(means_file,'w') as f:
-            print('meanID,redMean,greenMean,blueMean,redStd,greenStd,blueStd', file = f)
-            for row in means.itertuples():
-                print(row.Index + ',' + str(row.MeanR) + ',' + str(row.MeanG) + ',' + str(row.MeanB) + ',' + str(row.StdR) + ',' + str(row.StdG) + ',' + str(row.StdB), file = f)
+        print('calculate mean file')
+        if not os.path.exists(means_file):
+            with open(means_all_file, 'w') as f:
+                print('Clip,MeanR,MeanG,MeanB,StdR,StdG,StdB', file = f)
+                for video in os.listdir(videos_temp):
+                    video_folder = os.path.join(videos_temp,video)
+                    image_indices = []
+                    frames = []
+                    for image_file_name in os.listdir(video_folder):
+                        image_file_path = os.path.join(video_folder,image_file_name)
+                        if 'image' not in image_file_name:
+                            continue
+                        image_indices.append(int(image_file_name[6:11]))
+                        frames.append(image_file_path)
+                    image_indices.sort(reverse=True)
+                    n_frames = image_indices[0]
+                    with open(os.path.join(video_folder, 'n_frames'), 'w') as dst_file:
+                        dst_file.write(str(n_frames))
+                    img = io.imread(frames[0])
+                    mean = img.mean(axis = (0,1))
+                    std = img.std(axis = (0,1))
+                    print(video + ',' + ','.join([str(x) for x in mean]) + ',' + ','.join([str(x) for x in std]), file = f)
+            dt = pd.read_csv(means_all_file,sep=',')
+            annotation_df = pd.read_csv(annotation_file,sep=',')
+            dt['MeanID'] = dt.apply(lambda row: annotation_df.loc[annotation_df.Location==row.Clip].MeanID.values[0], axis = 1)
+            means = dt.groupby('MeanID').mean()
+
+            with open(means_file,'w') as f:
+                print('meanID,redMean,greenMean,blueMean,redStd,greenStd,blueStd', file = f)
+                for row in means.itertuples():
+                    print(row.Index + ',' + str(row.MeanR) + ',' + str(row.MeanG) + ',' + str(row.MeanB) + ',' + str(row.StdR) + ',' + str(row.StdG) + ',' + str(row.StdB), file = f)
 
         train_list = os.path.join(self.args.Results_directory,'train_list.txt')
         val_list = os.path.join(self.args.Results_directory,'val_list.txt')
         test_list = os.path.join(self.args.Results_directory,'test_list.txt')
         test_animals = [self.args.TEST_PROJECT]
-        with open(train_list,'w') as train,open(val_list,'w') as val, open(test_list,'w') as test:
-            for index,row in annotation_df.iterrows():
-                animal = row.MeanID.split(':')[0]
-                if animal in test_animals:
-                    print(row.Location+','+row.Label,file=test)
-                else:
-                    if np.random.uniform()<0.8:
-                        print(row.Location+','+row.Label,file=train)
+        if not os.path.exists(train_list):
+            with open(train_list,'w') as train,open(val_list,'w') as val, open(test_list,'w') as test:
+                for index,row in annotation_df.iterrows():
+                    animal = row.MeanID.split(':')[0]
+                    if animal in test_animals:
+                        print(row.Location+','+row.Label,file=test)
                     else:
-                        print(row.Location+','+row.Label,file=val)
+                        if np.random.uniform()<0.8:
+                            print(row.Location+','+row.Label,file=train)
+                        else:
+                            print(row.Location+','+row.Label,file=val)
         
     def prepare_json(self):
         train_list = os.path.join(self.args.Results_directory,'train_list.txt')
         val_list = os.path.join(self.args.Results_directory,'val_list.txt')
         test_list = os.path.join(self.args.Results_directory,'test_list.txt')
-        source_json_path = os.path.join(self.args.Log_directory,'source.json')
-        
+        source_json_path = os.path.join(self.args.Results_directory,'source.json')
+        if os.path.exists(source_json_path):
+            return
         def convert_csv_to_dict(csv_path, subset):
             keys = []
             key_labels = []
@@ -121,6 +124,7 @@ class DP_worker():
 
     def work(self):
         self.prepare_data()
+        print('data conversion done, preparing json file')
         self.prepare_json()
         
         
