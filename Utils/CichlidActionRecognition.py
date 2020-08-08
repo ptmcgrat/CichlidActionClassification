d895adad14de9ab7e30d756545bf3031abb6aad3
Lijiang Long <llong35@gatech.edu>
2020-08-07 21:42:26 -0400

    debug

--- Utils/CichlidActionRecognition.py
+++ Utils/CichlidActionRecognition.py
@@ -188,45 +188,44 @@ class ML_model():
         for i, (inputs, targets) in enumerate(data_loader):
             data_time.update(time.time() - end_time)
 
-            if not opt.no_cuda:
-                targets = targets.cuda(async=True)
-                inputs = Variable(inputs)
-                targets = Variable(targets)
-                outputs = model(inputs)
-                loss = criterion(outputs, targets)
-                acc = calculate_accuracy(outputs, targets)
+            targets = targets.cuda(async=True)
+            inputs = Variable(inputs)
+            targets = Variable(targets)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            acc = calculate_accuracy(outputs, targets)
 
-                losses.update(loss.data, inputs.size(0))
-                accuracies.update(acc, inputs.size(0))
+            losses.update(loss.data, inputs.size(0))
+            accuracies.update(acc, inputs.size(0))
 
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-                batch_time.update(time.time() - end_time)
-                end_time = time.time()
+            batch_time.update(time.time() - end_time)
+            end_time = time.time()
 
-                batch_logger.log({
-                    'epoch': epoch,
-                    'batch': i + 1,
-                    'iter': (epoch - 1) * len(data_loader) + (i + 1),
-                    'loss': losses.val,
-                    'acc': accuracies.val,
-                    'lr': optimizer.param_groups[0]['lr']
-                })
+            batch_logger.log({
+                'epoch': epoch,
+                'batch': i + 1,
+                'iter': (epoch - 1) * len(data_loader) + (i + 1),
+                'loss': losses.val,
+                'acc': accuracies.val,
+                'lr': optimizer.param_groups[0]['lr']
+            })
 
-                print('Epoch: [{0}][{1}/{2}]\t'
-                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                      'Acc {acc.val:.3f} ({acc.avg:.3f})'.format(
-                    epoch,
-                    i + 1,
-                    len(data_loader),
-                    batch_time=batch_time,
-                    data_time=data_time,
-                    loss=losses,
-                    acc=accuracies))
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc {acc.val:.3f} ({acc.avg:.3f})'.format(
+                epoch,
+                i + 1,
+                len(data_loader),
+                batch_time=batch_time,
+                data_time=data_time,
+                loss=losses,
+                acc=accuracies))
             epoch_logger.log({
                 'epoch': epoch,
                 'loss': losses.avg,
@@ -266,44 +265,43 @@ class ML_model():
         # pdb.set_trace()
         for i, (inputs, targets) in enumerate(data_loader):
             data_time.update(time.time() - end_time)
+            
+            targets = targets.cuda(async=True)
+            with torch.no_grad():
+                inputs = Variable(inputs)
+                targets = Variable(targets)
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+                acc = calculate_accuracy(outputs, targets)
+                #########  temp line, needs to be removed##################################
+                #             for j in range(len(targets)):
+                #                 confidence_for_each_validation[paths[j]] = [x.item() for x in outputs[j]]
+                '''
+                rows = [int(x) for x in targets]
+                columns = [int(x) for x in np.argmax(outputs,1)]
+                assert len(rows) == len(columns)
+                for idx in range(len(rows)):
+                    confusion_matrix[rows[idx]][columns[idx]] +=1
+                '''
+                ###########################################################################
+                losses.update(loss.data, inputs.size(0))
+                accuracies.update(acc, inputs.size(0))
 
-            if not opt.no_cuda:
-                targets = targets.cuda(async=True)
-                with torch.no_grad():
-                    inputs = Variable(inputs)
-                    targets = Variable(targets)
-                    outputs = model(inputs)
-                    loss = criterion(outputs, targets)
-                    acc = calculate_accuracy(outputs, targets)
-                    #########  temp line, needs to be removed##################################
-                    #             for j in range(len(targets)):
-                    #                 confidence_for_each_validation[paths[j]] = [x.item() for x in outputs[j]]
-                    '''
-                    rows = [int(x) for x in targets]
-                    columns = [int(x) for x in np.argmax(outputs,1)]
-                    assert len(rows) == len(columns)
-                    for idx in range(len(rows)):
-                        confusion_matrix[rows[idx]][columns[idx]] +=1
-                    '''
-                    ###########################################################################
-                    losses.update(loss.data, inputs.size(0))
-                    accuracies.update(acc, inputs.size(0))
+                batch_time.update(time.time() - end_time)
+                end_time = time.time()
 
-                    batch_time.update(time.time() - end_time)
-                    end_time = time.time()
-
-                    print('Epoch: [{0}][{1}/{2}]\t'
-                          'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                          'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                          'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                          'Acc {acc.val:.3f} ({acc.avg:.3f})'.format(
-                        epoch,
-                        i + 1,
-                        len(data_loader),
-                        batch_time=batch_time,
-                        data_time=data_time,
-                        loss=losses,
-                        acc=accuracies))
+                print('Epoch: [{0}][{1}/{2}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Acc {acc.val:.3f} ({acc.avg:.3f})'.format(
+                    epoch,
+                    i + 1,
+                    len(data_loader),
+                    batch_time=batch_time,
+                    data_time=data_time,
+                    loss=losses,
+                    acc=accuracies))
             #########  temp line, needs to be removed##################################
             # print(confusion_matrix)
             # confusion_matrix = pd.DataFrame(confusion_matrix)
