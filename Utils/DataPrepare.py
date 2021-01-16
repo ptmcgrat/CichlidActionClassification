e28c6361cfdbde72c62f13eb757cb4020fb81747
Lijiang Long <lijianglong@ipsec-172-16-77-37.vpn.gatech.edu>
2021-01-16 00:34:38 -0500

    update

--- Utils/DataPrepare.py
+++ Utils/DataPrepare.py
@@ -171,7 +171,7 @@ class DP_worker():
         val_list = os.path.join(self.args.Results_directory,'val_list.txt')
         test_list = os.path.join(self.args.Results_directory,'test_list.txt')
         source_json_path = os.path.join(self.args.Results_directory,'source.json')
-        if args.Purpose == 'Classify':
+        if self.args.Purpose == 'Classify':
             with open(args.Train_json,'r') as input_f:
                 training_json = json.load(input_f) 
             dst_data = {}
