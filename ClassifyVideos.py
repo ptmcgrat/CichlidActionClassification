bb84ba4a47820994bbed1d5af2138f63544fc883
Lijiang Long <lijianglong@ipsec-172-16-77-190.vpn.gatech.edu>
2021-01-20 23:39:59 -0500

    test

--- ClassifyVideos.py
+++ ClassifyVideos.py
@@ -4,7 +4,7 @@ from Utils.DataPrepare import DP_worker
 
 
 parser = argparse.ArgumentParser(description='This script takes a model, and apply this model to new video clips')
-
+# Input data
 parser.add_argument('--ML_videos_directory',
                     type = str, 
                     default = '/data/home/llong35/data/labeled_videos',
@@ -20,6 +20,16 @@ parser.add_argument('--Train_json',
                     type = str, 
                     default = '/data/home/llong35/temp/test_JAN_7_temp/source.json',
                     help = 'json file previously used for training')
+                    
+parser.add_argument('--Train_log',
+                    type = str, 
+                    default = os.path.join(os.getenv("HOME"),'temp','test_JAN_20_log'),
+                    help = 'log file in training')
+
+parser.add_argument('--Output_file',
+                    type = str, 
+                    default = '/data/home/llong35/temp/output.csv',
+                    help = 'csv file that keeps the confidence and label for each video clip')
                     
 parser.add_argument('--Purpose',
                     type = str, 
@@ -39,12 +49,12 @@ parser.add_argument('--resume_path',
                     default='/data/home/llong35/temp/test_JAN_7_temp/save_50.pth',
                     type=str,
                     help='Save data (.pth) of previous training')
+                    
+parser.add_argument('--n_threads',
+                    default=5,
+                    type=int,
+                    help='Number of threads for multi-thread loading')
 
-# Output data
-parser.add_argument('--Results_directory',
-                    type = str,
-                    default=os.path.join(os.getenv("HOME"),'temp','test_JAN_15_temp'),
-                    help = 'directory to store sample prepare logs')
 
 # Parameters for the dataloader
 parser.add_argument('--sample_duration',
@@ -57,10 +67,7 @@ parser.add_argument('--sample_size',
                     type=int,
                     help='Height and width of inputs')
                     
-parser.add_argument('--n_threads',
-                    default=5,
-                    type=int,
-                    help='Number of threads for multi-thread loading')
+
 
 
 # Parameters for the optimizer
@@ -95,7 +102,7 @@ parser.add_argument('--n_epochs',default=200,type=int,help='Number of total epoc
 
 
 args = parser.parse_args()
-
+# Parameters to load from previous training_log
 
 def check_args(args):
     if not os.path.exists(args.Results_directory):
