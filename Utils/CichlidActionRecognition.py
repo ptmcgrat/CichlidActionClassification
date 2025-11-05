ede3088919226af395db69d858a616441cc6dc12
Patrick McGrath <pmcgrath7@ipsec-10-2-226-177.vpn.gatech.edu>
2025-11-05 07:57:00 -0500

    Small updates

--- Utils/CichlidActionRecognition.py
+++ Utils/CichlidActionRecognition.py
@@ -211,7 +211,7 @@ class ML_model():
                 confusion_matrix_file = os.path.join(self.args.Results_directory,'epoch_{epoch}_confusion_matrix.csv'.format(epoch=i))
                 confusion_matrix.to_csv(confusion_matrix_file)
                 validation_results_file = os.path.join(self.args.Results_directory,'epoch_{epoch}_results.csv'.format(epoch=i))
-                # pdb.set_trace()
+                pdb.set_trace()
                 results_df.to_csv(validation_results_file)
 
                 scheduler.step(validation_loss)
