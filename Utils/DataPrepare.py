abb8e0760a4ae4fcad3fe9627782969755eccf94
Patrick McGrath <pmcgrath7@ipsec-10-2-98-80.vpn.gatech.edu>
2025-11-06 14:50:30 -0500

    Small updates

--- Utils/DataPrepare.py
+++ Utils/DataPrepare.py
@@ -54,6 +54,7 @@ class DP_worker():
                 output = subprocess.run(cmd, capture_output = True)
                 if output.returncode != 0:
                     pdb.set_trace()
+        pdb.set_trace()
         print(str(sum(self.dt.ClipAvailable == False)) + ' videos not available and will be excluded')
 
     def _calculateMeans(self):
