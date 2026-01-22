c51af824fea1c3e24aa0598a363e4467ada22627
Patrick McGrath <pmcgrath7@ipsec-10-2-224-252.vpn.gatech.edu>
2026-01-22 15:42:33 -0500

    Small updates

--- Utils/DataPrepare.py
+++ Utils/DataPrepare.py
@@ -87,7 +87,7 @@ class DP_worker():
 
     def prepareJson(self, purpose, json_file, n_classes):
         
-        assert purpose is in ['classify','train']
+        assert purpose in ['classify','train']
 
         dst_data = {}
 
