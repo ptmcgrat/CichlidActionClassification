5effee69a1e8f118d43d66383c56d344b1389eeb
Patrick McGrath <pmcgrath7@ipsec-10-2-224-252.vpn.gatech.edu>
2026-01-22 18:55:49 -0500

    Small updates

--- Utils/data_loader.py
+++ Utils/data_loader.py
@@ -19,9 +19,8 @@ class cichlids(data.Dataset):
 
     def readDatabase(self):
         with open(self.json_file,'r') as input_f:
-            training_json = json.load(input_f) 
-
-        data_dict = json.load(input_f)
+            data_dict = json.load(input_f)
+            
         self.labels = data_dict['labels']
         self.labels_to_idx = {x:i for x,i in zip(self.labels,range(len(self.labels)))}
         self.clip_dict = {k:v for k,v in data_dict['database'].items() if v['subset'] == self.dataset_type}
